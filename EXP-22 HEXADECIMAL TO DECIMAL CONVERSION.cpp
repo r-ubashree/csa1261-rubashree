4d692cdc6c5cc48f405f2a384b3ffdb4c65af1a9
r-ubashree <142959532+r-ubashree@users.noreply.github.com>
2023-08-23 08:50:07 +0530

    Add files via upload

---
+++ EXP-22 HEXADECIMAL TO DECIMAL CONVERSION.cpp
@@ -0,0 +1,11 @@
+//HEXADECIMAL TO DECIMAL CONVERSION
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the hex decimal number");
+	scanf("%x",&n);
+	printf("the decimal value is:%d",n);
+	return 0;
+}
+
---
+++ EXP-31 DECIMAL TO HEXADECIMAL.cpp
@@ -0,0 +1,10 @@
+//DECIMAL TO HEXADECIMAL
+#include <stdio.h>
+int main()
+{
+	int decimalNumber;
+	printf("Enter the decimal no:");
+	scanf("%d",&decimalNumber);
+    printf("Hexadecimal number is: %X", decimalNumber);
+    return 0;
+}
